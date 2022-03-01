901ac2280138b89b8154ca87157ff34148e52663
DreyIMM <andreydario26@gmail.com>
2022-02-28 22:28:04 -0300

    Criando function uploadTask e tratando os arquivos

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -23,11 +23,65 @@ class DropBoxController{
 
         //esse evento altera o modal de carregamento
         this.inputFilesEl.addEventListener('change', event=>{
-            // console.log(event.target.files); -> coleção do arquivo enviado
+            //(event.target.files) -> coleção do arquivo enviado
+            
+            this.uploadTask(event.target.files);
             this.snackModalEl.style.display = 'block';
+
+
+
         
         });
 
     }
 
+    //metodo para receber os arquivos
+    // file não é um array e sim uma coleção e para converta, utiliza-se o spreatch linha 44
+    //para cada solicitação vamos utilizar o ajax
+    uploadTask(files){
+
+        let promises = [];
+        [...files].forEach(file =>{
+
+            promises.push(new Promise((resolve,reject)=>{
+
+                let ajax = new XMLHttpRequest();
+
+                //abrir a conecção via post na posta /upload   
+                ajax.open('POST', '/upload');
+                //.onload é onde o funcionamento ocorre
+                ajax.onload = event =>{
+
+                    try{
+
+                        resolve(JSON.parse(ajax.responseText));
+
+                    }catch(e){
+
+                        reject(e);
+                    }
+
+                };
+
+                //caso tenha erro no envio
+                ajax.onerror = event =>{
+
+                    reject(event);
+
+                };
+                // como é leitura de arquivo, para se lê, utiliza o FormData
+                let formData = new FormData();
+                //1) nome do campo que o post receba - 2) qual é o arquivo que será enviado (file do forEach)
+                formData.append('input-file', file);
+
+
+                //envia as informações via AJAX
+                ajax.send(formData);               
+
+            }));
+        });
+        //verifica todo mundo, e os resolve da colection
+        return Promise.all(promises)
+
+    }
 }
