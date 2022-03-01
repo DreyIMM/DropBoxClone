0024b1bef3c7014253d7d6f6834cb36e9f00df73
DreyIMM <andreydario26@gmail.com>
2022-02-28 23:05:26 -0300

    Incluind formidable para leitura armazenar o arquivo em uma pasta

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
+const formidable = require('formidable');
 var router = express.Router();
+var fomidable = require('formidable');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+router.post('/upload', (req,res) =>{
+
+  //metodo Incomingo form(recuperando o formulario)
+  //parametros (1- ./upload (raiz vai na pasta upload) 2 mantém a extensão do arquivo
+  let form = new formidable.IncomingForm({
+    uploadDir: './upload',
+    keepExtensions: true
+  });
+  //Faz o parse dos dados que estão chegando (interpreta) ulti parans são os arquivos, filds
+  form.parse(req,(err,fields, files)=>{
+
+    res.json({
+      files
+    });
+
+  });
+
+
+ 
+})
 
 module.exports = router;
